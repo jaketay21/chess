6155c5757a48a1116e0e4dc69b455d424b31bf95
jacob <jaketay221@gmail.com>
2025-09-19 14:40:37 -0600

    pass all king move tests

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -6,14 +6,42 @@ import java.util.Collection;
 import java.util.List;
 
 public class KingMoveCalculator extends ChessMoveCalculator {
-    public KingMoveCalculator() {}
+    private List<ChessMove> possibleMoves;
+    public KingMoveCalculator() {
+        possibleMoves = new ArrayList<>();
+    }
+
+    private void addToList(ChessPosition start, ChessPosition next, ChessBoard board, ChessGame.TeamColor color){
+        if(positionValid(next)){
+            if(!empty(board,next)){
+                ChessPiece blocker = board.getPiece(next);
+                if(blocker.getTeamColor() != color){
+                    possibleMoves.add(new ChessMove(start, next, null));
+                }
+            }else{
+                possibleMoves.add(new ChessMove(start, next, null));
+            }
+        }
+    }
 
     @Override
     public Collection<ChessMove> CalculateMove(ChessBoard board, ChessGame.TeamColor color, ChessPiece.PieceType promotionType, ChessPosition startPosition){
-        List<ChessMove> possibleMoves = new ArrayList<>();
-
-
-
+        ChessPosition up = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn());
+        ChessPosition upL = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() - 1);
+        ChessPosition Left = new ChessPosition(startPosition.getRow() , startPosition.getColumn() -1);
+        ChessPosition downL = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() - 1);
+        ChessPosition down = new ChessPosition(startPosition.getRow() -1, startPosition.getColumn());
+        ChessPosition downR = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() + 1);
+        ChessPosition Right = new ChessPosition(startPosition.getRow(), startPosition.getColumn() + 1);
+        ChessPosition upR = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() + 1);
+        addToList(startPosition,up,board,color);
+        addToList(startPosition,upL,board,color);
+        addToList(startPosition,Left,board,color);
+        addToList(startPosition,downL,board,color);
+        addToList(startPosition,down,board,color);
+        addToList(startPosition,downR,board,color);
+        addToList(startPosition,Right,board,color);
+        addToList(startPosition,upR,board,color);
         return possibleMoves;
     }
 
